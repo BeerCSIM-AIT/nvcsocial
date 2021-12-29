9088a75e231ed7cd7804f04cb34540f8a2efd0cd
BeerCSIM_AIT <st121032@ait.ac.th>
2021-12-29 14:31:46 +0700

    edit db connection

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
-    $host="localhost:3306";
-    $user="root";
-    $pass="12345678";
+    $host="nvc-db.c5lttagztqjo.ap-southeast-1.rds.amazonaws.com";
+    $user="nvc_social";
+    $pass="nvc_social2021";
     $dbname="nvc_social";
     $conn = mysqli_connect($host,$user,$pass,$dbname);
     if(!$conn){
