0a8bb55e283d888f5fad9bf4bd4c0a18491e0b6c
anurak18844 <anurak18844@gmail.com>
2022-01-02 20:11:03 +0700

    add manageuser pages! finish

--- admin/manageuser.php
+++ admin/manageuser.php
@@ -7,15 +7,15 @@
 </div>
 <hr>
 <div class="container mb-5">
-    <!-- PHP SELECT NEW USESRS -->
     <?php 
-        $sql = "SELECT * FROM users WHERE status = 'new'";
-        $qNewUser = mysqli_query($conn, $sql);
-        $rows = mysqli_num_rows($qNewUser);
+        $status = array("new","approved","rejected","banned");
+        foreach($status as $s){
+            $sql = "SELECT * FROM users WHERE status = '$s'";
+            $query = mysqli_query($conn, $sql);
+            $rows = mysqli_num_rows($query);
     ?>
-    <!-- New Users -->
-    <h3><label for="newusers" class="col-sm-12 col-form-label">New Users.</label></h3>
-    <table class="table text-center">
+        <h3><label for="" class="col-sm-12 col-form-label"><?php titleShow($s);?></label></h3>
+        <table class="table text-center">
         <thead>
             <tr>
                 <th>Id</th>
@@ -26,14 +26,13 @@
             </tr>
         </thead>
         <tbody>
-            <!-- Check User in status new -->
             <?php 
                 if($rows==0){
             ?> 
             <tr>
             <td colspan="5">
                 <div class="container text-center">
-                    <h4>Don't have new usesr</h4>
+                    <h4>No Items</h4>
                 </div>   
             </td>   
             </tr> 
@@ -41,7 +40,7 @@
             }
                 else{
                 for($i=0; $i<$rows; $i++){
-                $result = mysqli_fetch_array($qNewUser);
+                $result = mysqli_fetch_array($query);
                 $id = $result['id'];
             ?>
                 <tr>
@@ -50,8 +49,7 @@
                 <td><?php echo $result['lastName'];?></td>
                 <td><?php echo $result['email'];?></td>
                 <td style="width:250px;">
-                    <a href="index.php?menu=manageuser&action=rejected&idAction=<?php echo $id;?>" class="btn btn-danger">Reject</a>
-                    <a href="index.php?menu=manageuser&action=approved&idAction=<?php echo $id;?>" class="btn btn-success">Approve</a>
+                <?php btnShow($s, $id);?>
                 </td>
                 </tr>
             <?php
@@ -61,178 +59,55 @@
         </tbody>
         
     </table>
-
-
-
-    <!-- PHP SELECT APPROVE -->
-    <?php 
-        $sql = "SELECT * FROM users WHERE status = 'approved'";
-        $qApproveUser = mysqli_query($conn, $sql);
-        $rows = mysqli_num_rows($qApproveUser);
+    <?php
+        }        
     ?>
-    <!-- Approved Users -->
-    <h3><label for="approvedusers" class="col-sm-12 col-form-label">Approved Users.</label></h3>
-    <table class="table text-center">
-        <thead>
-            <tr>
-                <th>Id</th>
-                <th>Firstname</th>
-                <th>Lastname</th>
-                <th>E-Mail</th>
-                <th>Action</th>
-            </tr>
-        </thead>
-        <tbody>
-            <!-- Check User in status aprove -->
-            <?php 
-                if($rows==0){
-            ?> 
-            <tr>
-            <td colspan="5">
-                <div class="container text-center">
-                    <h4>Don't have approve usesr</h4>
-                </div>   
-            </td>
-            </tr>
-                    
-            <?php
-                }else{
-                for($i=0; $i<$rows; $i++){
-                $result = mysqli_fetch_array($qApproveUser);
-                $id = $result['id'];
-            ?>
-            <tr>
-                <td><?php echo  $id;?></td>
-                <td><?php echo $result['firstName'];?></td>
-                <td><?php echo $result['lastName'];?></td>
-                <td><?php echo $result['email'];?></td>
-                <td style="width:250px;">
-                    <a href="index.php?menu=manageuser&action=banned&idAction=<?php echo $id;?>" class="btn btn-danger">Ban user</a>
-                </td>
-            </tr>
-            <?php
-                }
-            }
-            ?>
-        </tbody>
-    </table>
-
-
-    <!-- PHP SELECT REJECT USERS -->
-    <?php 
-        $sql = "SELECT * FROM users WHERE status = 'rejected'";
-        $qRejectUser = mysqli_query($conn, $sql);
-        $rows = mysqli_num_rows($qRejectUser);
-    ?>
-
-    <!-- Rejected Users -->
-    <h3><label for="rejectusers" class="col-sm-12 col-form-label">Reject Users.</label></h3>
-    <table class="table text-center">
-        <thead>
-            <tr>
-                <th>Id</th>
-                <th>Firstname</th>
-                <th>Lastname</th>
-                <th>E-Mail</th>
-                <th>Action</th>
-            </tr>
-        </thead>
-        <tbody>
-          <!-- Check User in status rejected -->
-          <?php 
-                if($rows==0){
-            ?> 
-            <tr>
-            <td colspan="5">
-                <div class="container text-center">
-                    <h4>Don't have rejected user</h4>
-                </div>   
-            </td>
-            </tr>
-                    
-            <?php
-                }else{
-                for($i=0; $i<$rows; $i++){
-                $result = mysqli_fetch_array($qRejectUser);
-                $id = $result['id'];
-            ?>
-            <tr>
-                <td><?php echo  $id;?></td>
-                <td><?php echo $result['firstName'];?></td>
-                <td><?php echo $result['lastName'];?></td>
-                <td><?php echo $result['email'];?></td>
-                <td style="width:250px;">
-                    <a href="index.php?menu=manageuser&action=new&idAction=<?php echo $id;?>" class="btn btn-info text-light">Retry</a>
-                    <a href="index.php?menu=manageuser&action=approved&idAction=<?php echo $id;?>" class="btn btn-success">Approve</a>
-                </td>
-            </tr>
-            <?php
-                }
-            }
-            ?>
-        </tbody>
-    </table>
-
-    <!-- PHP SELECT NEW REKECT USERS -->
-    <?php 
-        $sql = "SELECT * FROM users WHERE status = 'banned'";
-        $qBanned = mysqli_query($conn, $sql);
-        $rows = mysqli_num_rows($qBanned);
-    ?>
-    <!-- Baned Users -->
-    <h3><label for="banusers" class="col-sm-12 col-form-label">Baner Users.</label></h3>
-    <table class="table text-center">
-        <thead>
-            <tr>
-                <th>Id</th>
-                <th>Firstname</th>
-                <th>Lastname</th>
-                <th>E-Mail</th>
-                <th>Action</th>
-            </tr>
-        </thead>
-        <tbody>
-            <!-- Check User in status banned -->
-            <?php 
-                if($rows==0){
-            ?> 
-            <tr>
-            <td colspan="5">
-                <div class="container text-center">
-                    <h4>Don't have banned user</h4>
-                </div>   
-            </td>   
-            </tr>
-            <?php
-                }else{
-                for($i=0; $i<$rows; $i++){
-                $result = mysqli_fetch_array($qBanned);
-                $id = $result['id'];
-            ?>
-            <tr>
-                <td><?php echo $id;?></td>
-                <td><?php echo $result['firstName'];?></td>
-                <td><?php echo $result['lastName'];?></td>
-                <td><?php echo $result['email'];?></td>
-                <td style="width:250px;">
-                    <a href="index.php?menu=manageuser&action=approved&idAction=<?php echo $id;?>" class="btn btn-danger">Un Ban</a>
-                </td>
-            </tr>
-            <?php
-                }
-            }
-            ?>
-        </tbody>
-    </table>
 </div>
 
-<?php 
+
+<?php
     if(isset($_GET['action'])){
         $status = $_GET['action'];
         $idAction = $_GET['idAction'];
-            $sql = "UPDATE users SET status ='$status' WHERE id = $idAction";
-            $qUpdateStatus = mysqli_query($conn, $sql);
-            echo "<script>window.alert('Success!');
-            window.location.href = 'index.php?menu=manageuser';</script>";
+        $sql = "UPDATE users SET status ='$status' WHERE id = $idAction";
+        $qUpdateStatus = mysqli_query($conn, $sql);
+        echo "<script>window.alert('Success!');
+        window.location.href = 'index.php?menu=manageuser';</script>";
+    }
+
+    function titleShow($s){
+        switch($s){
+            case "new":
+                echo "New User";
+                break;
+            case "approved":
+                echo "Approve User";
+                break;
+            case "rejected":
+                echo "Reject User";
+                break;
+            default:
+                echo "banned";
+                break;
+        }
+    }
+
+    function btnShow($status,$id){
+        switch($status){
+            case "new":
+                echo "<a href='index.php?menu=manageuser&action=rejected&idAction=$id' class='btn btn-danger me-1'>Reject</a>";
+                echo "<a href='index.php?menu=manageuser&action=approved&idAction=$id' class='btn btn-success'>Approve</a>";
+                break;
+            case "approved":
+                echo "<a href='index.php?menu=manageuser&action=banned&idAction=$id' class='btn btn-danger'>Ban user</a>";
+                break;
+            case "rejected":
+                echo "<a href='index.php?menu=manageuser&action=new&idAction=$id' class='btn btn-info text-light me-1'>Retry</a>";
+                echo "<a href='index.php?menu=manageuser&action=approved&idAction=$id' class='btn btn-success'>Approve</a>";
+                break;
+            default:
+                echo "<a href='index.php?menu=manageuser&action=approved&idAction=$id' class='btn btn-danger'>Un Ban</a>";
+                break;
+        }
     }
 ?>
